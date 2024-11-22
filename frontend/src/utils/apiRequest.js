0142d1ec226898722d2407062686025f84b79575
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2024-11-23 00:50:08 +0530

    fixing auth errors globally

--- frontend/src/utils/apiRequest.js
+++ frontend/src/utils/apiRequest.js
@@ -9,4 +9,24 @@ const apiRequest = axios.create({
   },
 });
 
+// adding token dynamically
+apiRequest.interceptors.request.use((config) => {
+  const token = localStorage.getItem('jwt-shoobestate');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+// Handle errors globally
+apiRequest.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API error:", error);
+    return Promise.reject(error);
+  }
+);
+
 export default apiRequest;
