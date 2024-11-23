5b4c467d47eeb1b7174a97529622b002aff2960c
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2024-11-23 12:11:52 +0530

    fix leaflet's marker png not appearing

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -3,7 +3,23 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
-import { latLngBounds } from 'leaflet'
+import { latLngBounds, Icon } from 'leaflet'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Configure default icon
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
 
 const Map = ({items}) => {  
   // Default center (fallback coordinates)
