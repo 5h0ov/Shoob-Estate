9aec8c7c2b68ee3149dfa636cf5dc3d541cab4c9
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2024-11-26 13:22:04 +0530

    trying to fix uploading of file in serverless env like vercel

--- backend/middleware/uploadFile.js
+++ backend/middleware/uploadFile.js
@@ -1,23 +1,17 @@
 import multer from "multer";
-import fs from "fs";
-
-// Create uploads directory if it doesn't exist
-const uploadDir = "./uploads";
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 // set storage
-const storage = multer.diskStorage({
-  // desitnation
-  destination: function (req, res, cb) {
-    cb(null, uploadDir);
-  },
-  // filename
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now() + file.originalname);
-  },
-});
+// const storage = multer.diskStorage({
+//   // desitnation
+//   destination: function (req, res, cb) {
+//     cb(null, "./uploads/");
+//   },
+//   // filename
+//   filename: function (req, file, cb) {
+//     cb(null, file.fieldname + "-" + Date.now() + file.originalname);
+//   },
+// });
+const storage = multer.memoryStorage();
 
 const filerFilter = (req, file, cb) => {
   cb(null, true);
